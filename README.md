c2bfe0c8a40fd4862d1cdcbe742327f1bd7f742a
aa668086 <893588207@qq.com>
2017-10-01 21:10:27 +0800

    lundun

---
+++ README.md
@@ -0,0 +1,73 @@
+# RootHelper
+An extension to use libsuperuser library more easily
+
+Requirements
+------------
+MinAPI is 9, and the device has to be rooted in order to perform root operations (obviously...).
+
+Example
+-------
+If you have Android API 11 and above, this will show the recent tasks :
+
+    new Thread(){
+        @Override
+        public void run() {
+            final Root root = Root.getInstance();
+            final boolean gotRoot = root.getRoot();
+            if(gotRoot)
+                root.runCommands("input keyevent " + KeyEvent.KEYCODE_APP_SWITCH);
+        }
+    }.start();
+
+And the result:
+
+![enter image description here](https://raw.githubusercontent.com/AndroidDeveloperLB/RootHelper/master/extras/demo.gif)
+
+The sample in this project will just show that the count of files in a protected path is positive for root operations, yet 0 for normal API. 
+
+Note that it's adviced to show the user some kind of progress bar while getting the root privilege, because on some ROMs, it could take some time till the user sees a dialog asking if it's ok to grant your app root privilege.
+
+How to import via gradle
+------------------------
+Import in gradle using :
+
+root gradle file:
+
+	allprojects {
+		repositories {
+			...
+			maven { url "https://jitpack.io" }
+		}
+	}
+
+your app's gradle file:
+
+	dependencies {
+	        compile 'com.github.AndroidDeveloperLB:RootHelper:#'
+	}
+
+where "#" is the latest release of the library, as shown on [Jitpack's website](https://jitpack.io/#AndroidDeveloperLB/RootHelper/).
+
+
+How to use
+----------
+You have a global singleton Root:
+
+    Root root=Root.getInstance();
+
+With it, you first have to gain root privilege from the user, and might take some time. You have 2 options for gaining root privilege:
+
+    public boolean getRoot()
+    or    
+    public void getRoot(final IGotRootListener listener)
+
+The first can only be used in background thread, and the other can be called on the UI thread.
+
+After you got root privilege, you can perform root operations (only on background thread), using any of those :
+
+    public List<String> runCommands(@NonNull final List<String> commands)
+    or
+    public List<String> runCommands(@NonNull final String... commands)
+
+You give them a list of commands to run, and they will return you a list of the output from the commands (or null in case of any error, like missing root privilege).
+
---
+++ demo/src/main/java/com/lb/root_helper_demo/MainActivity.java
@@ -0,0 +1,153 @@
+package com.lb.root_helper_demo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+
+import com.lb.root_helper.lib.Root;
+
+import java.io.File;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String PROTECTED_PATH_TO_TEST = "/data/";
+    public static final int LOADER_ID = 1;
+    private final Root mRoot = Root.getInstance();
+    private View mProgressBar;
+    private View mRootButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.normalApiButton).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                final String[] list = new File(PROTECTED_PATH_TO_TEST).list();
+                int listSize = list == null ? 0 : list.length;
+                Toast.makeText(MainActivity.this, "在保护路径上找到文件计数：" + listSize, Toast.LENGTH_SHORT).show();
+            }
+        });
+        mProgressBar = findViewById(R.id.progressBar);
+        mRootButton = findViewById(R.id.rootButton);
+        initLoader(false);
+        mRootButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                initLoader(true);
+            }
+        });
+    }
+
+    public void lundun(View view){
+        new Thread(){
+            @Override
+            public void run() {
+                final Root root = Root.getInstance();
+                final boolean gotRoot = root.getRoot();
+                if(gotRoot)
+                    root.runCommands("input keyevent " + KeyEvent.KEYCODE_APP_SWITCH);
+            }
+        }.start();
+    }
+
+    private void initLoader(boolean forceStart) {
+        final LoaderManager loaderManager = getSupportLoaderManager();
+        final RootLoader previousLoader = (RootLoader) (Loader<?>) loaderManager.getLoader(LOADER_ID);
+        if (previousLoader != null && forceStart)
+            loaderManager.destroyLoader(LOADER_ID);
+        mRootButton.setEnabled(true);
+        mProgressBar.setVisibility(View.GONE);
+        if (forceStart || previousLoader != null) {
+            mRootButton.setEnabled(false);
+            mProgressBar.setVisibility(View.VISIBLE);
+            loaderManager.initLoader(LOADER_ID, null, new LoaderCallbacks<Integer>() {
+                @Override
+                public Loader<Integer> onCreateLoader(final int id, final Bundle args) {
+                    return new RootLoader(MainActivity.this);
+                }
+
+                @Override
+                public void onLoadFinished(final Loader<Integer> loader, final Integer result) {
+                    loaderManager.destroyLoader(LOADER_ID);
+                    mRootButton.setEnabled(true);
+                    mProgressBar.setVisibility(View.GONE);
+                    if (result == null)
+                        Toast.makeText(MainActivity.this, "根未获得，因此无法执行测试。", Toast.LENGTH_SHORT).show();
+                    else
+                        Toast.makeText(MainActivity.this, "在保护路径上找到文件计数：" + result, Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onLoaderReset(final Loader<Integer> loader) {
+                }
+            }).forceLoad();
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        String url = null;
+        switch (item.getItemId()) {
+            case R.id.menuItem_all_my_apps:
+                url = "https://play.google.com/store/apps/developer?id=AndroidDeveloperLB";
+                break;
+            case R.id.menuItem_all_my_repositories:
+                url = "https://github.com/AndroidDeveloperLB";
+                break;
+            case R.id.menuItem_current_repository_website:
+                url = "https://github.com/AndroidDeveloperLB/RootHelper";
+                break;
+        }
+        if (url == null)
+            return true;
+        final Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+        startActivity(intent);
+        return true;
+    }
+
+    private static class RootLoader extends AsyncTaskLoader<java.lang.Integer> {
+
+        public RootLoader(final Context context) {
+            super(context);
+        }
+
+        @Override
+        public java.lang.Integer loadInBackground() {
+            final Root root = Root.getInstance();
+            final boolean gotRoot = root.getRoot();
+            if (!gotRoot)
+                return null;
+            final List<String> result = root.runCommands("ls " + PROTECTED_PATH_TO_TEST);
+            if (result == null)
+                return 0;
+            //Log.d("AppLog", "result of the command:");
+            //for (String line : result)
+            //    Log.d("AppLog", line);
+            return result.size();
+        }
+    }
+
+
+}
